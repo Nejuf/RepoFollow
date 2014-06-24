2a5ec295ce8827dad7bd354db4a4073374c89f06
Nejuf <nejuf1@gmail.com>
2014-06-23 21:40:18 -0700

    Adds pagination for public repo search results

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -4,6 +4,9 @@ class GithubController < ApplicationController
   def public_repos
     if params[:search].present?
       @repos = Octokit.search_repos(q: params[:search]).items
+    elsif params[:q].present?
+      parsed_query = JSON.parse(URI::decode(params[:q]).sub('=>', ':'))
+      @repos = Octokit.search_repos(parsed_query, page: params[:page]).items
     else
       since = params[:since] || 0
       @repos = Octokit.all_repositories(since: since)
