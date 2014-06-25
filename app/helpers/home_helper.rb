a3d7910a745dee65b94ad77959093fc7c2281014
Nejuf <nejuf1@gmail.com>
2014-06-25 12:05:08 -0700

    Adds messaging for when there is no activity or followed repositories

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def has_feed_entries?
+    @feed_dates.any? { |date_info| date_info[:commits].present?  }
+  end
+
+  def showing_recent_activity?
+    Date.parse(@feed_dates.last[:date]) >= Date.today
+  end
 end
